d738a9376fbfd84d40b04fcb45b5fb3b9c5a15f6
zhushuaikang <shuaikang.zhu@vhall.com>
2022-10-18 20:34:41 +0800

    test: 11

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ createApp({
          <SButton color="green" round plain icon="edit" ></SButton>
         <SButton color="gray" round plain icon="check" ></SButton>
         <SButton color="yellow" round plain icon="message"></SButton>
-         <SButton color="red" round plain icon="delete" ></SButton>
+          <SButton color="red" round plain icon="delete" ></SButton>
         </div>
 
     `,
