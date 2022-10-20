b84fa992a70a15f1c70015af5ea173b990a06adf
zhushuaikang <shuaikang.zhu@vhall.com>
2022-10-20 14:22:27 +0800

    test: 2

--- packages/shuangwaiwai433/src/entry.ts
+++ packages/shuangwaiwai433/src/entry.ts
@@ -1,7 +1,7 @@
 import { App } from "vue";
 import MyButton from "./button";
 import SFCButton from "./SFCButton.vue";
-// import JSXButton from "./JsxButton";
+// import JSXButton from "./JSXButton";
 
 // 导出单独组件
 export { MyButton, SFCButton };
